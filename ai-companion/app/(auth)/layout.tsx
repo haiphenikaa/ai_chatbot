33a9e7bf35fbf4e0f31184b4ed21d0c0d5e5128e
Đinh Bá Hải <22010126@st.phenikaa-uni.edu.vn>
2024-09-19 00:35:49 +0700

    authentication

--- ai-companion/app/(auth)/layout.tsx
+++ ai-companion/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ const AuthLayout = ({
     children: React.ReactNode;  
 }) => {
     return ( 
-        <div className="flex place-items-center justify-center h-full">
+        <div className="flex items-center justify-center h-full">
             {children}
         </div>
      );
